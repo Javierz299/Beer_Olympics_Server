d2a3031da46c2744b715ddc5800cec92fb75baaa
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-06-24 11:25:45 -0400

    tables migrated to pgADMIN

--- migrations/001.do.create.players.sql
+++ migrations/001.do.create.players.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "players"(
     "player_id" SERIAL PRIMARY KEY,
-    "full_name" VARCHAR(32) NOT NULL,
+    "full_name" VARCHAR(32) UNIQUE NOT NULL,
     "nickname" VARCHAR(20) UNIQUE NOT NULL,
-    "password" INT(4) NOT NULL
+    "password" INT NOT NULL
 );
--- migrations/003.do.create.games.sql
+++ migrations/003.do.create.games.sql
@@ -2,5 +2,5 @@ CREATE TABLE "games"(
     "games_id" SERIAL PRIMARY KEY,
     "game_name" VARCHAR(100) NOT NULL,
     "team_name" VARCHAR references "teams"(team_name) ON DELETE CASCADE NOT NULL,
-    "player_name" VARCHAR references ""()
+    "player_name" VARCHAR references "teams"(player_name) ON DELETE CASCADE NOT NULL
 );
