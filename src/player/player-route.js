2a2cfd1a04945898268a069399a21b33673e31ea
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-06-24 13:17:57 -0400

    error http headers sent

--- src/player/player-route.js
+++ src/player/player-route.js
@@ -6,19 +6,34 @@ const pool = require('../db')
 
 PlayerRouter
     .post('/', jsonBodyParser, async (req,res,next) => {
-        const r = await req.body
-        const values = [r.full_name, r.nickname, r.password]
-        pool.query(`INSERT INTO players(full_name, nickname, password)
-            VALUES($1, $2, $3, $4)`,
-            values,
-            (q_err, q_res) => {
-                if(q_err){
-                    return next(q_err)
-                }
-                console.log(res.json(q_res.rows))
-            }
 
-        )
-    })
+        const values = await [req.body.full_name, req.body.nickname, req.body.password]
+        console.log(values)
+
+        try{
+        await pool.query(`INSERT INTO players(full_name, nickname, password)
+        VALUES($1, $2, $3)`,
+        values,
+        (q_err, q_res) => {
+            if(!q_err){
+                console.log('json reached')
+                return res.json(q_res.rows)
+            } else {
+                console.log('reached',q_err)
+                return next(q_err)
+            }
+            
+        }
+    
+    )
+    next()
+} catch(err){
+    next(err)
+}
+        
+})
+
+
+
 
     module.exports = PlayerRouter
